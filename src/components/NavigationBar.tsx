6a38805e65757ad7f750aae498e4e396b8671fab
Andre <andrezekic@me.com>
2024-04-13 23:37:30 -0400

    update about page

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,16 +1,21 @@
-import { Link } from "react-router-dom";
-import { Button, Flex, Spacer } from "@chakra-ui/react";
+import { Link as ReactRouterLink } from "react-router-dom";
+import { Button, Flex, Spacer, Heading, Link } from "@chakra-ui/react";
 
 export function NavigationBar() {
   const isRoot = window.location.pathname === "/";
 
   return (
-    <Flex p={2} width="100vw" position="absolute">
+    <Flex p={2} width="100vw" position="absolute" alignItems="center">
+      <Heading size="md" px={3}>
+        <Link as={ReactRouterLink} to={"/"}>
+          Andre Zekic
+        </Link>
+      </Heading>
       <Spacer />
-      <Button as={Link} to={isRoot ? "about" : "/"} variant="ghost">
+      <Button as={ReactRouterLink} to={isRoot ? "about" : "/"} variant="ghost">
         {isRoot ? "About" : "Home"}
       </Button>
-      <Button as={Link} to="mailto:andre@zekic.ca" variant="ghost">
+      <Button as={ReactRouterLink} to="mailto:andre@zekic.ca" variant="ghost">
         Contact
       </Button>
     </Flex>
--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,16 +1,30 @@
 import { PageWrapper } from "../PageWrapper";
-import { Heading, Link, Icon } from "@chakra-ui/react";
+import { Heading, Link, Icon, HStack, Stack } from "@chakra-ui/react";
 import { FaGithub } from "react-icons/fa";
 
 export function About() {
+  const introduction =
+    "I am a software developer based in Canada with a passion for creating great user experiences.";
+  const biography =
+    "I have built large-scale, full-stack web applications across industries ranging from e-commerce to big data and finance, within roles at industry leaders including CARFAX Canada, SSENSE, and Altitude Sports. I've worked with a wide range of technologies, and I have extensive experience with AWS, TypeScript, React, and Node.js.";
+  const conclusion =
+    "If you would like to get in touch, please feel free to reach out to me at ";
+
   return (
     <PageWrapper>
-      <Heading m="2" textAlign="center">
-        Andre is a software developer based in Canada.
-      </Heading>
-      <Link href="https://github.com/azekic" isExternal>
-        <Icon as={FaGithub} boxSize={12} />
-      </Link>
+      <Stack m="6" maxWidth="42rem" gap={4}>
+        <Heading size="lg">{introduction}</Heading>
+        <Heading size="lg">{biography}</Heading>
+        <Heading size="lg">
+          {conclusion}
+          <Link href="mailto:andre@zekic.ca">andre@zekic.ca</Link>.
+        </Heading>
+      </Stack>
+      <HStack gap={4}>
+        <Link href="https://github.com/azekic" isExternal h="100%">
+          <Icon as={FaGithub} boxSize={12} />
+        </Link>
+      </HStack>
     </PageWrapper>
   );
 }
